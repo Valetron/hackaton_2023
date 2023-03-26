ce91821b6edb67bc5df75701fc112e389276c9a2
Andrey <null>
2023-03-27 00:53:20 +0300

    Тригер на insert

--- scripts/hackaton.sql
+++ scripts/hackaton.sql
@@ -17,3 +17,17 @@ CREATE TABLE IF NOT EXISTS event
 	FOREIGN KEY(camera_id) REFERENCES camera(id)
 );
 
+
+CREATE OR REPLACE FUNCTION notify_realtime() 
+	RETURNS trigger as $BODY$
+BEGIN
+	PERFORM pg_notify('new_notify',(NEW.description,', ', NEW.time_stamp,
+									(SELECT c.name FROM camera c 
+									 WHERE NEW.camera_id = c.id)::varchar)::varchar);
+	RETURN NULL;
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER updated_realtime_trigger AFTER INSERT ON event
+FOR EACH ROW EXECUTE PROCEDURE notify_realtime();
