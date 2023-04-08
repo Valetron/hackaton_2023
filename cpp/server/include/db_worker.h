d8776b42c2803fcdc8bf9979fb9b7c438603b066
Valetron <notspammeplz@gmail.com>
2023-04-08 22:29:23 +0300

    Работаю, мужики

--- cpp/server/include/db_worker.h
+++ cpp/server/include/db_worker.h
@@ -11,7 +11,7 @@ namespace
 static const std::map<std::string, std::string> PREPARED_STATEMENTS
 {
         {"select_camera", "select * from camera where id = $1"},
-        {"select_event", "select * from from event where id = $1"},
+        {"select_event", "select * from event where id = $1"},
         {"insert_camera", "insert into camera (name, processing_period, stream, areas)"
                           "VALUES ($1, $2, $3, $4)"},
         {"insert_event", "insert into event (camera_id, description, time_stamp, photo)"
@@ -23,12 +23,12 @@ class DBWorker final
 {
 public:
     DBWorker(const std::string& dataBaseCredentials);
-    std::string getData(const std::string& tableName);
+    std::string getData(const std::string& tableName, const int id);
     void setData(const std::string& tableName, const std::string& rawData);
 
 private:
     void checkConnection(const std::string& dataBaseCredentials);
 
 private:
-    pqxx::connection _connection;
+    std::unique_ptr<pqxx::connection> _connection;
 };
--- cpp/server/src/db_worker.cpp
+++ cpp/server/src/db_worker.cpp
@@ -3,16 +3,36 @@
 DBWorker::DBWorker(const std::string& dataBaseCredentials)
 {
     checkConnection(dataBaseCredentials);
+//    initSocket();
 }
 
 void DBWorker::checkConnection(const std::string& dataBaseCredentials)
 {
-    _connection = pqxx::connection(dataBaseCredentials);
+    _connection = std::make_unique<pqxx::connection>(dataBaseCredentials);
 
-    if (!_connection.is_open())
+    if (!_connection->is_open())
         throw std::runtime_error("Can't connect to database");
 
     for (const auto& [name, query] : PREPARED_STATEMENTS)
-        _connection.prepare(name, query);
+        _connection->prepare(name, query);
+}
 
+std::string DBWorker::getData(const std::string& tableName, const int id)
+{
+    pqxx::work txn(*_connection);
+    pqxx::result res;
+
+    if ("camera" == tableName)
+        res = txn.exec_prepared("select_camera", id);
+    else if ("event" == tableName)
+        res = txn.exec_prepared("select_event", id);
+    else
+        return std::string();
+
+    txn.commit();
+
+    if (res.empty())
+        return std::string();
+
+    return pqxx::to_string(res.at(0).at(0));
 }
